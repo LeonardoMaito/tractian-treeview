b4f9a5eab0bb5f065df785a52f23941e169784e6
Leonardo Maito <leonardot_m@hotmail.com>
2024-08-09 19:16:03 -0300

    fix: move repository files to the correct directories

--- lib/features/asset_page/data/repository/asset_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:treeview/features/asset_page/domain/models/asset_model.dart';
-
-abstract class AssetRepository{
-  Future<List<AssetModel>> getAssets();
-
-}
--- lib/features/asset_page/data/repository/location_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:treeview/features/asset_page/domain/models/location_model.dart';
-
-abstract class LocationRepository{
-  Future<List<LocationModel>> getLocations();
-
-}
