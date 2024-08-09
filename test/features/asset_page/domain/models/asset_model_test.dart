c4d205fad4f7f0fac32094bf8a995e81da359fb4
Leonardo Maito <leonardot_m@hotmail.com>
2024-08-09 19:06:20 -0300

    test: add test for creating asset model from json

--- test/features/asset_page/domain/models/asset_model_test.dart
+++ test/features/asset_page/domain/models/asset_model_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:treeview/features/asset_page/domain/models/asset_model.dart';
 
 void main() {
-  group('AssetModel Tests', () {
     test('Create AssetModel from JSON', (){
       final jaguarAssetJson = {
         "name": "CONVEYOR BELT ASSEMBLY",
@@ -19,5 +18,4 @@ void main() {
       expect(jaguarAsset.name, 'CONVEYOR BELT ASSEMBLY');
       expect(jaguarAsset.locationId, '656a07b3f2d4a1001e2144bf');
     });
-  });
 }
