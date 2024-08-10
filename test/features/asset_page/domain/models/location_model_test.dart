5f65b5382e0c1d7815a6edebdc392d32a1e9f9d8
Leonardo Maito <leonardot_m@hotmail.com>
2024-08-10 14:29:54 -0300

    test: add test for checking if Location is subLocation

--- test/features/asset_page/domain/models/location_model_test.dart
+++ test/features/asset_page/domain/models/location_model_test.dart
@@ -16,4 +16,32 @@ void main() {
     expect(jaguarLocation.parentId, '65674204664c41001e91ecb4');
     expect(jaguarLocation, isA<LocationModel>());
   });
+
+  test('Create LocationModel subLocation from JSON', (){
+
+    final jaguarLocationJson = {
+      "name": "PRODUCTION AREA - RAW MATERIAL",
+      "id": "65674204664c41001e91ecb4",
+      "parentId": null
+
+    };
+
+    final jaguarLocationTwoJson = {
+      "name": "CHARCOAL STORAGE SECTOR",
+      "id": "656a07b3f2d4a1001e2144bf",
+      "parentId": "65674204664c41001e91ecb4"
+    };
+
+
+    final jaguarLocation = LocationModel.fromJson(jaguarLocationJson);
+    final jaguarLocationTwo = LocationModel.fromJson(jaguarLocationTwoJson);
+
+    if(jaguarLocationTwo.parentId == jaguarLocation.id){
+      final newSubLocationList = List<LocationModel>.from(jaguarLocation.subLocations)
+        ..add(jaguarLocationTwo);
+      final updatedJaguarAsset = jaguarLocation.copyWith(subLocations: newSubLocationList);
+      expect(updatedJaguarAsset.subLocations.length, 1);
+      expect(updatedJaguarAsset.subLocations.first.id, jaguarLocationTwo.id);
+    }
+  });
 }
