7bc30e29d4c4294612e0a8f737d49e51d2662105
Leonardo Maito <leonardot_m@hotmail.com>
2024-08-10 16:28:19 -0300

    test: add missing test for component list attribute

--- test/features/asset_page/domain/models/asset_model_test.dart
+++ test/features/asset_page/domain/models/asset_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:treeview/features/asset_page/domain/models/asset_model.dart';
+import 'package:treeview/features/asset_page/domain/models/component_model.dart';
 
 void main() {
     test('Create AssetModel from JSON', (){
@@ -43,12 +44,42 @@ void main() {
       final jaguarAsset = AssetModel.fromJson(jaguarAssetJson);
       final jaguarAssetTwo = AssetModel.fromJson(jaguarAssetTwoJson);
 
-      if(jaguarAssetTwo.parentId == jaguarAsset.id){
-        final newSubAssetList = List<AssetModel>.from(jaguarAsset.subAssets)
+      final newSubAssetList = List<AssetModel>.from(jaguarAsset.subAssets)
+        ..add(jaguarAssetTwo);
+      final updatedJaguarAsset = jaguarAsset.copyWith(subAssets: newSubAssetList);
+      expect(updatedJaguarAsset.subAssets.length, 1);
+      expect(updatedJaguarAsset.subAssets.first.id, jaguarAssetTwo.id);
+
+    });
+
+    test('Create AssetModel components from JSON', (){
+
+      final jaguarAssetJson = {
+        "name": "MOTOR TC01 COAL UNLOADING AF02",
+        "id": "656a07c3f2d4a1001e2144c5",
+        "locationId": null,
+        "parentId": "656a07bbf2d4a1001e2144c2",
+        "sensorType": null,
+        "status": null
+      };
+
+      final jaguarAssetTwoJson = {
+        "name": "MOTOR RT COAL AF01",
+        "id": "656a07cdc50ec9001e84167b",
+        "locationId": null,
+        "parentId": "656a07c3f2d4a1001e2144c5",
+        "sensorType": "vibration",
+        "status": "operating"
+      };
+
+      final jaguarAsset = AssetModel.fromJson(jaguarAssetJson);
+      final jaguarAssetTwo = ComponentModel.fromJson(jaguarAssetTwoJson);
+
+      final newComponentList = List<ComponentModel>.from(jaguarAsset.components)
           ..add(jaguarAssetTwo);
-        final updatedJaguarAsset = jaguarAsset.copyWith(subAssets: newSubAssetList);
-        expect(updatedJaguarAsset.subAssets.length, 1);
-        expect(updatedJaguarAsset.subAssets.first.id, jaguarAssetTwo.id);
-      }
+      final updatedJaguarAsset = jaguarAsset.copyWith(components: newComponentList);
+      expect(updatedJaguarAsset.components.length, 1);
+      expect(updatedJaguarAsset.components.first.id, jaguarAssetTwo.id);
+
     });
 }
