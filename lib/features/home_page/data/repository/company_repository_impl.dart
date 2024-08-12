d80525e5de46dca861be6b588a9f973e701a8ac8
Leonardo Maito <leonardot_m@hotmail.com>
2024-08-12 12:57:06 -0300

    fix: class now implements company repository instead of extending

--- lib/features/home_page/data/repository/company_repository_impl.dart
+++ lib/features/home_page/data/repository/company_repository_impl.dart
@@ -2,7 +2,7 @@ import 'package:treeview/features/home_page/data/datasource/company_datasource.d
 import 'package:treeview/features/home_page/domain/models/company_model.dart';
 import 'package:treeview/features/home_page/domain/repository/company_repository.dart';
 
-class CompanyRepositoryImpl extends CompanyRepository{
+class CompanyRepositoryImpl implements CompanyRepository{
   final CompanyDatasource dataSource;
 
   CompanyRepositoryImpl(this.dataSource);
