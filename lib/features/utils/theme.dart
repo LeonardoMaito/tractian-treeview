ef6b86f499d357efd6d38fc4c5727a3bba3cb0b1
Leonardo Maito <leonardot_m@hotmail.com>
2024-08-12 12:58:09 -0300

    style: add new theme

---
+++ lib/features/utils/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme{
+  static ThemeData customTheme = ThemeData(
+    brightness: Brightness.light,
+    useMaterial3: true,
+    appBarTheme: const AppBarTheme(color: Color(0xFF17192d)),
+    scaffoldBackgroundColor: Colors.white,
+    textSelectionTheme: const TextSelectionThemeData(
+        cursorColor: Colors.black,
+        selectionHandleColor: Colors.black),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          minimumSize: const WidgetStatePropertyAll<Size>(Size(150, 75)),
+          backgroundColor: const WidgetStatePropertyAll<Color>(
+              Color(0xFF2188ff)),
+          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+            const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+        )),
+  );
+}
